3411dc6588a1a887b9fe5d958e5ef60cc4078fd2
Ben Pevsner <git@bpev.me>
2024-11-05 21:23:36 +0900

    initial commit

---
+++ source/background.ts
@@ -0,0 +1,12 @@
+import type Chrome from "@bpev/bext/types/chrome";
+import browserAPI from "@bpev/bext";
+
+browserAPI.tabs.onUpdated.addListener(
+  (
+    _tabId: number,
+    _tabChangeInfo: Chrome.TabChangeInfo,
+    { url }: Chrome.Tab,
+  ) => {
+    console.log(url);
+  },
+);
